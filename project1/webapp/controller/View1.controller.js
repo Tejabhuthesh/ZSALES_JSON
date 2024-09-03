2c47f113c8751a94d85ed60318c20db91ecdcd8a
Tejabhuthesh Golla <tejabhutesh213@gmail.com>
2024-09-03 07:45:10 +0000

    first commit

---
+++ project1/webapp/controller/View1.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("idproject1.project1.controller.View1", {
+        onInit: function () {
+
+        }
+    });
+});
